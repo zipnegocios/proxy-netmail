b47ab4aad1493cb29b4d84e2be8b0a48ddaf3ccb
Gustavo Amarista <zipnegocios@gmail.com>
2026-04-06 07:13:05 -0400

    fix: server.js y package.json
    -cambio  "start": "bash start.sh",     por "start": "node server.js",
    -server.js script del server start

---
+++ server.js
@@ -0,0 +1,16 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const port = parseInt(process.env.PORT || '3000', 10);
+const app = next({ dir: './apps/web', dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, () => {
+    console.log(`> Next.js ready on http://localhost:${port}`);
+  });
+});
